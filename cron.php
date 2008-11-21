cb76dc716c443c7c6289626a38f92ea1dd99b5bc
tripark <atari900@WebRanger.local>
2008-11-21 16:28:48 -0600

    Intial commit

---
+++ cron.php
@@ -0,0 +1,13 @@
+<?php
+// $Id: cron.php,v 1.36 2006/08/09 07:42:55 dries Exp $
+
+/**
+ * @file
+ * Handles incoming requests to fire off regularly-scheduled tasks (cron jobs).
+ */
+
+ini_set("memory_limit","60M");
+
+include_once './includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+drupal_cron_run();
