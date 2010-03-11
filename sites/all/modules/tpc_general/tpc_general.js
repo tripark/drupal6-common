9b8688c013c043b778881260e3fffb1e53503a76
Les Lim <les@les-lims-imac.local>
2010-03-11 16:04:59 -0600

    Code in tpc_general.js to prevent CKEditor from assuming http in links

--- sites/all/modules/tpc_general/tpc_general.js
+++ sites/all/modules/tpc_general/tpc_general.js
@@ -12,7 +12,30 @@ $(document).ready(function() {
       return false;
     });
   });
+  
+  // Alter CKEditor's link dialog to make the protocol setting more obvious.
+  if (typeof(CKEDITOR) !== "undefined") {
+    CKEDITOR.on( 'dialogDefinition', function( ev ) {
+      // Take the dialog name and its definition from the event data.
+      var dialogName = ev.data.name;
+      var dialogDefinition = ev.data.definition;
+    
+      // Check if the definition is from the dialog we're
+      // interested on (the "Link" dialog).
+      if ( dialogName == 'link' ) {
+        // Get a reference to the "Link Info" tab.
+        var infoTab = dialogDefinition.getContents( 'info' );
+    
+        // Set the default value for the protocol field.
+        var protocolField = infoTab.get( 'protocol' );
+        protocolField['label'] = "Path type";
+        protocolField['items'][4][0] = "(relative)";
+        protocolField['default'] = '';
+      }
+    });
+  }
 });
+
 
 /**
  * NOTE:
