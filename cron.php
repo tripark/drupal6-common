291fec8ecf2e65fd69a6779dc2792c887e71fd5c
Les Lim <les@triangleparkcreative.com>
2011-01-23 02:36:08 -0600

    Update Drupal to 6.20

--- cron.php
+++ cron.php
@@ -6,8 +6,6 @@
  * Handles incoming requests to fire off regularly-scheduled tasks (cron jobs).
  */
 
-ini_set("memory_limit","120M");
-
 include_once './includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 drupal_cron_run();
